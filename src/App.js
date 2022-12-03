732db1c2c92e4717935d54acf155b982f0425210
RjH <ryanjayhuffaker@gmail.com>
2022-12-03 10:29:27 -0700

    Improved security

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
-const app = new Clarifai.App({
-  apiKey: "5675b0b21b9f45eda84cc78380127fd1"
-});
-
 const initialState = {
   input: '',
   imageUrl: '',
@@ -61,7 +57,14 @@ class App extends Component {
 
   onImageSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
     .then(response => {
       if(response){
         fetch('http://localhost:3000/image', {
